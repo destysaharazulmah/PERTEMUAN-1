7a27e3a2749d537e8e4c462f125d5983701c0d7b
destysaharazulmah <destysahara6@gmail.com>
2024-10-10 15:06:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$nama = "Desty";
+$umur = 21;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contoh Variabel PHP</title>
+</head>
+<body>
+    <h1>Halo, <?php echo $nama; ?>!</h1>
+    <p>Umur Anda adalah <?php echo $umur; ?> tahun.</p> 
+</body>
+</html>
